bbdb10931a2d2da388e5ab94d2cbec774ba8df4b
Alex Vashurov <AlexVashurov@MacBook-Pro-Alex.local>
2014-10-13 23:46:43 +0400

    Initial Commit

---
+++ Test 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test 4
+//
+//  Created by Alex Vashurov on 13.10.14.
+//  Copyright (c) 2014 Alex Vashurov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
